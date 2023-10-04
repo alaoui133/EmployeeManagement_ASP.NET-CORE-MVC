ea8556979d3e4b45c00fa3b0a38c37dedcfaf194
alaoui133 <alaoui133@gmail.com>
2023-10-04 23:06:28 +0100

    fix error

--- EmployeeManagement/Controllers/AdministrationController.cs
+++ EmployeeManagement/Controllers/AdministrationController.cs
@@ -1,4 +1,5 @@
-﻿using EmployeeManagement.ViewModels;
+﻿using EmployeeManagement.Models;
+using EmployeeManagement.ViewModels;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
@@ -7,12 +8,12 @@ namespace EmployeeManagement.Controllers
     public class AdministrationController : Controller
     {
         private readonly RoleManager<IdentityRole> _roleManager;
-        private readonly UserManager<IdentityUser> _userManager;
+        private readonly UserManager<AppUser> _userManager;
 
-
-        public AdministrationController(RoleManager<IdentityRole> roleManager)
+        public AdministrationController(RoleManager<IdentityRole> roleManager, UserManager<AppUser> userManager)
         {
             _roleManager = roleManager;
+            _userManager = userManager;
         }
         [HttpGet]
         public IActionResult CreateRole()
@@ -29,7 +30,7 @@ namespace EmployeeManagement.Controllers
                 IdentityRole role = new IdentityRole
                 {
                     Name = model.RoleName,
-                     
+
                 };
                 IdentityResult result = await _roleManager.CreateAsync(role);
 
@@ -53,23 +54,19 @@ namespace EmployeeManagement.Controllers
         public IActionResult ListRoles()
         {
             var roles = _roleManager.Roles;
-            return View(roles); 
-        }
-
-
-        [HttpPost]
-        public IActionResult ListRoles(int x)
-        {
-            var roles = _roleManager.Roles;
             return View(roles);
         }
 
+
+
+
+
         [HttpGet]
-        public async Task<IActionResult> Update(string id)
+        public async Task<IActionResult> Update(string? id)
         {
             if (id is null)
             {
-                return View("NotFound","Please Add Role Id to url");
+                return View("NotFound", "Please Add Role Id to url");
             }
             IdentityRole role = await _roleManager.FindByIdAsync(id);
             if (role == null)
@@ -80,27 +77,85 @@ namespace EmployeeManagement.Controllers
             UpdateRoleViewModel model = new UpdateRoleViewModel()
             {
                 Id = role.Id,
-                RoleName = role.Name
+                RoleName = role.Name,
+                Users = new List<string>()
 
             };
-
-            foreach (var user in _userManager.Users )
+            var list = _userManager.Users.ToList();
+            foreach (AppUser user in list)
             {
-                if (await _userManager.IsInRoleAsync(user , role.Name))
+
+                if (await _userManager.IsInRoleAsync(user, role.Name))
                 {
                     model.Users.Add(user.Email);
                 }
             }
 
-            return View();
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Update(UpdateRoleViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+
+
+                IdentityRole role = await _roleManager.FindByIdAsync(model.Id);
+                if (role == null)
+                {
+                    return View("NotFound", $"this Role Id :{model.Id} Note Exist");
+                }
+
+                role.Name = model.RoleName;
+                IdentityResult result = await _roleManager.UpdateAsync(role);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction(nameof(ListRoles));
+                }
+
+            }
+            return View(model);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> UpdateUsersRole(string idRole)
+        {
+
+            IdentityRole role = await _roleManager.FindByIdAsync(idRole);
+            if (role == null)
+            {
+                return View("NotFound", $"this Role Id :{role.Id} Note Exist");
+            }
+
+            List<UpdateUsersRoleViewModel> ModelList = new List<UpdateUsersRoleViewModel>();
+
+            foreach (AppUser user in _userManager.Users.ToList())
+            {
+                UpdateUsersRoleViewModel model = new UpdateUsersRoleViewModel()
+                {
+                    UserId = user.Id,
+                    UserName = user.UserName,
+                    IsSelected = false
+                };
+
+                if (await _userManager.IsInRoleAsync(user,role.Name))
+                {
+                    ModelList.Add(model);
+                }
+            }
+
+            ViewBag.RoleId = idRole ;
+            return View(ModelList);
         }
 
 
 
 
-       
+
+
     }
-   
+
 
 }
 
---
+++ EmployeeManagement/ViewModels/UpdateUsersRoleViewModel.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Build.Framework;
+using System.ComponentModel.DataAnnotations;
+
+namespace EmployeeManagement.ViewModels
+{
+    public class UpdateUsersRoleViewModel
+    {
+        public string UserId { get; set; }
+     
+        public string UserName { get; set; }
+       public bool IsSelected { get; set; }
+
+    }
+
+}
